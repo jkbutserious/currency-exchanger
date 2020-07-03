a71cd18c5b41bd811e4de8c5ea4710349873e982
Jeff Kim <kim2jy@hotmail.com>
2020-07-02 21:40:06 -0700

    Add exported async function for API fetch request to currency-exchanger.js Business Logic file

--- src/currency-exchanger.js
+++ src/currency-exchanger.js
@@ -1,1 +1,15 @@
 // Business Logic
+export async function currencyExchange() {
+  try{
+    let exchangedAmount = await fetch(`https://pro.exchangerate-api.com/v6/{process.env.API_Key}/pair/USD/{targetCurrency}`);
+    let exchangedResponse;
+    if (exchangedAmount.ok && exchangedAmount.status == 200) {
+      exchangedResponse = await exchangedAmount.json();
+    } else {
+      exchangedResponse = false;
+    }
+    return exchangedResponse;
+  } catch(error) {
+    return false;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { currencyExchange } from './src/currency-exchanger.js';
 
-$(document).ready(function () {
-  // $('#submit-button').submit(function (event) {
-  //     event.preventDefault();
+async function getExchangedCurrency()
 
-  // });
-});
+$(document).ready(function() {
+  $(#"currency-form").click(function() {
+    const targetCurrency = //target-currency-radio-button.val()
+
+    
+  })
+})
