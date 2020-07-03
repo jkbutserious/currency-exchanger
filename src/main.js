bf3d9d3d354228c295e7c2ff0f7be3e9d7a2fb6d
Jeff Kim <kim2jy@hotmail.com>
2020-07-03 13:17:26 -0700

    Add code to main.js that will allow user to pass selected currency as object param to API response - fix code to allow function argument as object.param

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { currencyExchange } from './currency-exchanger.js';
 
-async function getExchangedCurrency(originCurrency) {
+async function getExchangedCurrency(currency, originCurrency) {
   const currencyResponse = await currencyExchange();
   if (currencyResponse === false) {
     $("#exchanged-currency").text("Something went wrong with your request"); // This will become a notification that displays the specific error
   }
   else {
-    let exchangeRate = parseFloat(currencyResponse.conversion_rates.CAD);
+    const exchangeRate = parseFloat(currencyResponse.conversion_rates);
     $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
-    console.log(currencyResponse.conversion_rates.CAD)
+    console.log(currencyResponse.conversion_rates)
     // $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
     $("#exchange-currency").text(`The exchanged amount is: ${originCurrency * exchangeRate}`);
   }
