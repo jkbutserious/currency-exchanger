737d36f45db2fcd9dffe5ae98fc490a10df7e55b
Jeff Kim <kim2jy@hotmail.com>
2020-07-03 14:46:32 -0700

    Begin adding code in Business Logic and UI Logic to display the specific error if the API call fails for any reason

--- src/main.js
+++ src/main.js
@@ -4,16 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { currencyExchange } from './currency-exchanger.js';
 
-async function getExchangedCurrency(currency, originCurrency) {
+async function getExchangedCurrency(_currencyCode, originCurrency) {
   const currencyResponse = await currencyExchange();
   if (currencyResponse === false) {
     $("#exchanged-currency").text("Something went wrong with your request"); // This will become a notification that displays the specific error
   }
   else {
-    const exchangeRate = parseFloat(currencyResponse.conversion_rates);
+    const exchangeRate = parseFloat(currencyResponse.conversion_rates._currencyCode);
+    
     $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
-    console.log(currencyResponse.conversion_rates)
-    // $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
+    console.log(exchangeRate.conversion_rates._currencyCode);
+    console.log(currencyResponse.conversion_rates.CAD);
+
+    $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
     $("#exchange-currency").text(`The exchanged amount is: ${originCurrency * exchangeRate}`);
   }
 }
@@ -21,9 +24,9 @@ async function getExchangedCurrency(currency, originCurrency) {
 $(document).ready(function() {
   $("#currency-form").submit(function(event) {
     event.preventDefault();
-    const usdAmount = parseInt($("#usd-amount").val());
+    const usdAmount = parseFloat($("#usd-amount").val());
     let targetCurrencyCode = $("#currency").val();
 
-    getExchangedCurrency(usdAmount, targetCurrencyCode);
+    getExchangedCurrency(targetCurrencyCode, usdAmount);
   });
 });
