1d59e62d6fcdb189d8662a04fa82d666ff0ee329
Jeff Kim <kim2jy@hotmail.com>
2020-07-02 22:25:13 -0700

    Add to UI Logic functions to text exchange rate and exchanged amount to HTML output

--- src/main.js
+++ src/main.js
@@ -4,20 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { currencyExchange } from './src/currency-exchanger.js';
 
-async function getExchangedCurrency() {
+async function getExchangedCurrency(currency) {
   const currencyResponse = await currencyExchange();
   if (currencyResponse === false) {
     $("#exchanged-currency").text("Something went wrong with your request") // This will become a notification that displays the specific error
   }
   else {
-    $("#exchange-currency").text(currencyResponse.`${targetCurrency}`)
+    $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${currencyResponse.}${currency}`) //explore ability to nest template literal expressions
+    $("#exchange-currency").text(`The exchanged amount is: ${}`)
   }
 }
 
 $(document).ready(function() {
-  $(#"currency-form").click(function() {
-    const targetCurrency = //target-currency-radio-button.val()
+  $(#"currency-form").submit(function(event) {
+    event.preventDefault();
+    const originCurrency = parseInt($("#usd-amount").val());
+    let targetCurrencyCode = $("#currency").val();
 
-    
+    getExchangedCurrency(targetCurrency);
   })
 })
