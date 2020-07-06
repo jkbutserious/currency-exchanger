4e48e2f9cfe3c1d3df01662528bc18aa8a79be89
Jeff Kim <kim2jy@hotmail.com>
2020-07-05 23:41:15 -0700

    Add CSS styling and bootstrap framework to create a more pleasing UI

--- src/main.js
+++ src/main.js
@@ -7,18 +7,18 @@ import { currencyExchange } from './currency-exchanger.js';
 async function getExchangedCurrency(currencyCode, originCurrency) {
   const currencyResponse = await currencyExchange();
   if (currencyResponse.status !== 200) {
-    $("#exchange-currency").text(`Sorry, something went wrong (Error code: ${currencyResponse.status})`); // This will become a notification that displays the specific error
+    $("#exchange-currency").text(`Sorry, something went wrong (Error code: ${currencyResponse.status})`);
   }
   else if (currencyResponse.content.conversion_rates.length === 0 || !currencyResponse.content.conversion_rates[currencyCode]) {
-    $("#exchange-currency").text("The currency " + currencyCode + " doesn't exist.")
+    $("#exchange-currency").text("I'm sorry, but that currency doesn't exist.")
   }
   else {
     const exchangeRate = currencyResponse.content.conversion_rates[currencyCode];
     
-    $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
+    $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate.toFixed(2)}`);
 
     $("#exchange-rate").text(`The current exchange rate from US Dollars is: ${exchangeRate}`);
-    $("#exchange-currency").text(`The exchanged amount is: ${originCurrency * exchangeRate}`);
+    $("#exchange-currency").text(`The exchanged amount is: ${(originCurrency * exchangeRate).toFixed(2)} ${currencyCode}`);
   }
 }
 
